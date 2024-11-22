c48d368eab0a07a8de5cd5b25ce171d7b20660b7
nelson <ralph_nelson25@milton.edu>
2024-11-21 20:47:32 -0500

    worked on api (authentification working)

--- src/API2.java
+++ src/API2.java
@@ -18,7 +18,57 @@ public class API2 {
     }
 
     public API2() throws IOException, ParseException {
-        URL url = new URL("https://api.sportradar.com/nba/trial/v8/en/players/8ec91366-faea-4196-bbfd-b8fab7434795/profile.json?api_key=ao7A9LW1o6c7uV7hvb675Km1lWQUXIofi9clqylx");
+        String teamid = leagueHierarchy();
+        teamStats(teamid);
+    }
+
+    public String leagueHierarchy() throws IOException, ParseException{
+        URL url = new URL("https://api.sportradar.com/nba/production/v8/en/league/hierarchy.json?api_key=ao7A9LW1o6c7uV7hvb675Km1lWQUXIofi9clqylx");
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        httpConn.setRequestMethod("GET");
+
+        httpConn.setRequestProperty("accept", "application/json");
+
+        InputStream responseStream = httpConn.getResponseCode() / 100 == 2
+                ? httpConn.getInputStream()
+                : httpConn.getErrorStream();
+        Scanner s = new Scanner(responseStream).useDelimiter("\\A");
+        String response = s.hasNext() ? s.next() : "";
+        System.out.println("Response" + response);
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(response);
+        System.out.println("JSON: " + jsonObject);
+
+        JSONArray conferences = (JSONArray) jsonObject.get("conferences");
+        JSONObject conf = (JSONObject) conferences.get(0);
+        System.out.println("conf: " + conf);
+
+        JSONArray divisions = (JSONArray) conf.get("divisions");
+        System.out.println("divisions: " + divisions);
+        JSONObject div = (JSONObject) divisions.get(0);
+
+        System.out.println("div: " + div);
+
+        JSONArray teamsArray = (JSONArray) div.get("teams");
+        JSONObject team = (JSONObject) teamsArray.get(0);
+
+        System.out.println("team" + team);
+        System.out.println("name: " + team.get("name"));
+
+        if(team.get("name").equals("Wizards")){
+
+            String id = (String) team.get("id");
+
+            System.out.println(id);
+            return id;
+        }
+        return "";
+
+    }
+
+    public void teamStats(String teamid) throws IOException, ParseException{
+        URL url = new URL("https://api.sportradar.com/nba/production/v8/en/seasons/2024/REG/teams/" + teamid + "/statistics.json?api_key=ao7A9LW1o6c7uV7hvb675Km1lWQUXIofi9clqylx");
         HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
         httpConn.setRequestMethod("GET");
 
@@ -36,13 +86,15 @@ public class API2 {
         JSONObject jsonObject = (JSONObject) parser.parse(response);
         System.out.println("JSON: " + jsonObject);
 
-        JSONObject teamInfo = (JSONObject) jsonObject.get("team");
-        System.out.println(teamInfo.get("name"));
 
+        JSONObject rec = (JSONObject) jsonObject.get (0);
+        JSONArray record = (JSONArray) rec.get ("own_record");
 
+        System.out.println(record);
+            }
         }
 
-    }
+
 
 
 
